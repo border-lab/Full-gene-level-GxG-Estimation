6f6a435fc142a277f138c47d1c023dbc6091035b
Ziyan_Zhang <ziyanzhang017315@Gmail.com>
2026-02-06 22:02:50 -0500

    update process_code

--- test_data/process_code.sh
+++ test_data/process_code.sh
@@ -1,326 +1,140 @@
-#####pick a continues 1000 snps  from chr1 chromosome to simulate the high-LD situation
-N=$(wc -l < chr1.bim)
-START=$(shuf -i 1-$((N-39999)) -n 1)
-END=$(( START + 39999 ))
-sed -n "${START},${END}p" chr1.bim | awk '{print $2}' > snp_block400000.txt
-plink --bfile chr1 \
-      --extract snp_block400000.txt \
-      --make-bed \
-      --out chr1_block400000
-
-plink --bfile chr1_block400000 --recode A --out chr1_block400000
-
 #####pick a random 1000 snps  from chr1 chromosome to simulate the low-LD situation
 plink --bfile chr1 --thin-count 1000 --make-bed --out chr1_sub1000
 plink --bfile chr1_sub1000 --recode A --out chr1_sub1000
 
+#####pick the whole dataset used for simulation
+N=$(wc -l < chr1.bim)
+START=$(shuf -i 1-$((N-99999)) -n 1)
+END=$(( START + 99999 ))
+sed -n "${START},${END}p" chr1.bim | awk '{print $2}' > snp_block10k.txt
+plink --bfile chr1 \
+      --extract snp_block10k.txt \
+      --make-bed \
+      --out chr1_block10k
+plink --bfile chr1_block10k --recode A --out chr1_block10k
 
-##### perform LD pruning with a threshold of R2 > 0.8
+
+####filter out rare varients (MAF > 0.05)
+plink --bfile chr1_block10k --maf 0.05 --make-bed --out chr1_block10k_maf005
+
+##### perform LD pruning with a threshold of R2 > 0.9 (common variant)
 plink \
-  --bfile chr1_block1000 \
-  --indep-pairwise 1000 5 0.8 \
-  --out snps_to_keep_08
+  --bfile chr1_block10k_maf005 \
+  --indep-pairwise 1000 5 0.9 \
+  --out snps_to_keep_09_maf005
 
 plink \
-  --bfile chr1_block1000 \
-  --extract snps_to_keep_08.prune.in \
+  --bfile chr1_block10k_maf005 \
+  --extract snps_to_keep_09_maf005.prune.in \
   --make-bed \
-  --out chr1_block1000_pruned_08
+  --out chr1_block10k_pruned_09_maf005
 
 plink \
-  --bfile chr1_block1000_pruned_08 \
+  --bfile chr1_block10k_pruned_09_maf005 \
   --recode A \
-  --out chr1_block1000_pruned_08
+  --out chr1_block10k_pruned_09_maf005
 
-##### perform LD pruning with a threshold of R2 > 0.9
+##### perform LD pruning with a threshold of R2 > 0.9 (All variant)
+
 plink \
-  --bfile chr1_block1000 \
+  --bfile chr1_block10k \
   --indep-pairwise 1000 5 0.9 \
   --out snps_to_keep_09
 
 plink \
-  --bfile chr1_block1000 \
+  --bfile chr1_block10k \
   --extract snps_to_keep_09.prune.in \
   --make-bed \
-  --out chr1_block1000_pruned_09
+  --out chr1_block10k_pruned_09
 
 plink \
-  --bfile chr1_block1000_pruned_09 \
+  --bfile chr1_block10k_pruned_09 \
   --recode A \
-  --out chr1_block1000_pruned_09
+  --out chr1_block10k_pruned_09
 
 
- ##### perform LD pruning for 3500 SNP with a threshold of R2 > 0.9
+
+
+##### perform LD pruning with a threshold of R2 > 0.5 (common variant)
 plink \
-  --bfile chr1_block3500 \
-  --indep-pairwise 3500 5 0.9 \
-  --out 3500snps_to_keep_09
+  --bfile chr1_block10k_maf005 \
+  --indep-pairwise 1000 5 0.5 \
+  --out snps_to_keep_05_maf005
+
 plink \
-  --bfile chr1_block3500 \
-  --extract 3500snps_to_keep_09.prune.in \
+  --bfile chr1_block10k_maf005 \
+  --extract snps_to_keep_05_maf005.prune.in \
   --make-bed \
-  --out chr1_block3500_pruned_09
+  --out chr1_block10k_pruned_05_maf005
 
 plink \
-  --bfile chr1_block3500_pruned_09 \
+  --bfile chr1_block10k_pruned_05_maf005 \
   --recode A \
-  --out chr1_block3500_pruned_09
+  --out chr1_block10k_pruned_05_maf005
 
 
-  ##### perform LD pruning for 10K SNP with a threshold of R2 > 0.5
-plink \
-  --bfile chr1_block10000 \
-  --indep-pairwise 10000 5 0.5 \
-  --out 10ksnps_to_keep_05
+
+##### perform LD pruning with a threshold of R2 > 0.5 (All variant)
 
 plink \
-  --bfile chr1_block10000 \
-  --extract 10ksnps_to_keep_05.prune.in \
+  --bfile chr1_block10k \
+  --indep-pairwise 1000 5 0.5 \
+  --out snps_to_keep_05
+
+plink \
+  --bfile chr1_block10k \
+  --extract snps_to_keep_05.prune.in \
   --make-bed \
-  --out chr1_block10000_pruned_05
+  --out chr1_block10k_pruned_05
 
 plink \
-  --bfile chr1_block10000_pruned_05 \
+  --bfile chr1_block10k_pruned_05 \
   --recode A \
-  --out chr1_block10000_pruned_05
+  --out chr1_block10k_pruned_05
 
-##### filter SNPs with MAF < 0.05
-plink --bfile chr1_block10000_pruned_05 --maf 0.05 --make-bed --out chr1_block10000_pruned_05_maf005
+
+
+##### perform LD pruning with a threshold of R2 > 0.2 (common variant)
+plink \
+  --bfile chr1_block10k_maf005 \
+  --indep-pairwise 1000 5 0.2 \
+  --out snps_to_keep_02_maf005
 
 plink \
-  --bfile chr1_block10000_pruned_05_maf005 \
-  --recode A \
-  --out chr1_block10000_pruned_05_maf005
-
-
-###### 2 by 2 by 3
-
-N=$(wc -l < chr1.bim)
-START=$(shuf -i 1-$((N-9999)) -n 1)
-END=$(( START + 9999 ))
-sed -n "${START},${END}p" chr1.bim | awk '{print $2}' > snp_block10000.txt
-plink --bfile chr1 \
-      --extract snp_block10000.txt \
-      --make-bed \
-      --out chr1_block10000
-
-plink --bfile chr1_block10000 --recode A --out chr1_block10000
-
-
-#######for LD > 0.9 pruning test
- ##### R2 > 0.9， small sample size, all maf.
-plink \
-  --bfile chr1_block1500 \
-  --indep-pairwise 1500 5 0.9 \
-  --out 1500snps_to_keep_09
-plink \
-  --bfile chr1_block1500 \
-  --extract 1500snps_to_keep_09.prune.in \
+  --bfile chr1_block10k_maf005 \
+  --extract snps_to_keep_02_maf005.prune.in \
   --make-bed \
-  --out chr1_block1500_pruned_09
+  --out chr1_block10k_pruned_02_maf005
 
 plink \
-  --bfile chr1_block1500_pruned_09 \
+  --bfile chr1_block10k_pruned_02_maf005 \
   --recode A \
-  --out chr1_block1500_pruned_09
+  --out chr1_block10k_pruned_02_maf005
 
 
- ##### R2 > 0.9， large sample size, all maf.
 
- plink \
-  --bfile chr1_block5000 \
-  --indep-pairwise 5000 5 0.9 \
-  --out 5000snps_to_keep_09
+##### perform LD pruning with a threshold of R2 > 0.2 (All variant)
 plink \
-  --bfile chr1_block5000 \
-  --extract 5000snps_to_keep_09.prune.in \
+  --bfile chr1_block10k \
+  --indep-pairwise 1000 5 0.2 \
+  --out snps_to_keep_02
+
+plink \
+  --bfile chr1_block10k \
+  --extract snps_to_keep_02.prune.in \
   --make-bed \
-  --out chr1_block5000_pruned_09
+  --out chr1_block10k_pruned_02
 
 plink \
-  --bfile chr1_block5000_pruned_09 \
+  --bfile chr1_block10k_pruned_02 \
   --recode A \
-  --out chr1_block5000_pruned_09
-
-
-#####  ##### R2 > 0.9， small sample size, common variant.
-plink --bfile chr1_block5000_pruned_09 --maf 0.05 --make-bed --out chr1_block5000_pruned_09_maf005
-
-plink \
-  --bfile chr1_block5000_pruned_09_maf005 \
-  --recode A \
-  --out chr1_block5000_pruned_09_maf005
-
-
-#####  ##### R2 > 0.9， large sample size, common variant.
-plink \
-  --bfile chr1_block10000 \
-  --indep-pairwise 10000 5 0.9 \
-  --out 10000snps_to_keep_09
-plink \
-  --bfile chr1_block10000 \
-  --extract 10000snps_to_keep_09.prune.in \
-  --make-bed \
-  --out chr1_block10000_pruned_09
-
-plink \
-  --bfile chr1_block10000_pruned_09 \
-  --recode A \
-  --out chr1_block10000_pruned_09
-
-
-plink --bfile chr1_block10000_pruned_09 --maf 0.05 --make-bed --out chr1_block10000_pruned_09_maf005
-
-plink \
-  --bfile chr1_block10000_pruned_09_maf005 \
-  --recode A \
-  --out chr1_block10000_pruned_09_maf005
-
-
-#######for LD > 0.5 pruning test
-
- ##### R2 > 0.5， small sample size, all variant.
-plink \
-  --bfile chr1_block4000 \
-  --indep-pairwise 4000 5 0.5 \
-  --out 4000snps_to_keep_05
-plink \
-  --bfile chr1_block4000 \
-  --extract 4000snps_to_keep_05.prune.in \
-  --make-bed \
-  --out chr1_block4000_pruned_05
-
-plink \
-  --bfile chr1_block4000_pruned_05 \
-  --recode A \
-  --out chr1_block4000_pruned_05
-
-##### R2 > 0.5， large sample size, all variant.
-plink \
-  --bfile chr1_block15000 \
-  --indep-pairwise 15000 5 0.5 \
-  --out 15000snps_to_keep_05
-
-plink \
-  --bfile chr1_block15000 \
-  --extract 15000snps_to_keep_05.prune.in \
-  --make-bed \
-  --out chr1_block15000_pruned_05
-
-plink \
-  --bfile chr1_block15000_pruned_05 \
-  --recode A \
-  --out chr1_block15000_pruned_05
-
-##### R2 > 0.5， small sample size, common variant.
-
-plink --bfile chr1_block15000_pruned_05 --maf 0.05 --make-bed --out chr1_block15000_pruned_05_maf005
-
-plink \
-  --bfile chr1_block15000_pruned_05_maf005 \
-  --recode A \
-  --out chr1_block15000_pruned_05_maf005
-
-##### R2 > 0.5， large sample size, common variant.
-
-plink \
-  --bfile chr1_block20000 \
-  --indep-pairwise 20000 5 0.5 \
-  --out 20000snps_to_keep_05
-
-plink \
-  --bfile chr1_block20000 \
-  --extract 20000snps_to_keep_05.prune.in \
-  --make-bed \
-  --out chr1_block20000_pruned_05
-
-plink \
-  --bfile chr1_block20000_pruned_05 \
-  --recode A \
-  --out chr1_block20000_pruned_05
-
-
-plink --bfile chr1_block20000_pruned_05 --maf 0.05 --make-bed --out chr1_block20000_pruned_05_maf005
-
-plink \
-  --bfile chr1_block20000_pruned_05_maf005 \
-  --recode A \
-  --out chr1_block20000_pruned_05_maf005
-
-  #######for LD > 0.2 pruning test
-
-   ##### R2 > 0.2， small sample size, all variant.
-plink \
-  --bfile chr1_block10000 \
-  --indep-pairwise 10000 5 0.2 \
-  --out 10000snps_to_keep_02
-
-plink \
-  --bfile chr1_block10000 \
-  --extract 10000snps_to_keep_02.prune.in \
-  --make-bed \
-  --out chr1_block10000_pruned_02
-
-plink \
-  --bfile chr1_block10000_pruned_02 \
-  --recode A \
-  --out chr1_block10000_pruned_02
-
-
-     ##### R2 > 0.2， large sample size, all variant.
-plink \
-  --bfile chr1_block20000 \
-  --indep-pairwise 20000 5 0.2 \
-  --out 20000snps_to_keep_02
-  
-plink \
-  --bfile chr1_block20000 \
-  --extract 20000snps_to_keep_02.prune.in \
-  --make-bed \
-  --out chr1_block20000_pruned_02
-
-plink \
-  --bfile chr1_block20000_pruned_02 \
-  --recode A \
-  --out chr1_block20000_pruned_02
-
-
-##### R2 > 0.2， small sample size, common variant.
-plink --bfile chr1_block20000_pruned_02 --maf 0.05 --make-bed --out chr1_block20000_pruned_02_maf005
-
-plink \
-  --bfile chr1_block20000_pruned_02_maf005 \
-  --recode A \
-  --out chr1_block20000_pruned_02_maf005
-
-
-  ##### R2 > 0.2， large sample size, common variant.
-
-plink --bfile chr1_first30000 --recode A --out chr1_first30000
-plink --bfile chr1_last30000 --recode A --out chr1_last30000
+  --out chr1_block10k_pruned_02
 
 
 
-plink \
-  --bfile chr1_last30000 \
-  --indep-pairwise 30000 5 0.2 \
-  --out last_30000snps_to_keep_02
-
-plink \
-  --bfile chr1_last30000 \
-  --extract last_30000snps_to_keep_02.prune.in \
-  --make-bed \
-  --out chr1_last30000_pruned_02
-
-plink \
-  --bfile chr1_last30000_pruned_02 \
-  --recode A \
-  --out chr1_last30000_pruned_02
+#### choose the dataset with R2 > 0.9 for common variant and all variant to simulate the high-LD situation
+cut -d' ' -f1-606 chr1_block10k_pruned_09.raw > highLD_small_all.raw
 
 
-plink --bfile chr1_last30000_pruned_02 --maf 0.05 --make-bed --out chr1_last30000_pruned_02_maf005
 
-plink \
-  --bfile chr1_last30000_pruned_02_maf005 \
-  --recode A \
-  --out chr1_last30000_pruned_02_maf005
+
